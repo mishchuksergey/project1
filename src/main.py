9a755854c91b36a508d680fde732c0f1b9647ac9
mishchuk <mishchuksg@gmail.com>
2024-11-17 22:05:27 +0300

    filter for russian names

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+import re
+
 def clear_names(file_name: str) -> list:
     """Функция для очистки имен от лишних символов"""
     new_name_lists = list()
-    with open ('date/' + file_name) as names_file:
+    with open ('data/' + file_name, 'r') as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
             new_name = ''
@@ -13,8 +15,23 @@ def clear_names(file_name: str) -> list:
     return new_name_lists
 
 
+def is_cyrillic(name_item: str) -> bool:
+    """Проверка на вхождение кирилицы в строку"""
+
+    return bool(re.search('[а-яА-Я]', name_item))
+
+def russian_names_list(names_list: list) ->list:
+    """Фильтрация имен написанных на русском"""
+    new_names_list = list()
+    for name_item in names_list:
+        if is_cyrillic(name_item):
+            new_names_list.append(name_item)
+    return new_names_list
+
+
+
 if __name__ == '__main__':
     cleared_name = clear_names('names.txt')
 
-    for i in cleared_name:
-        print(i)
+    print(russian_names_list(cleared_name))
+
