8fad66fb7a700f6109627ff48b0c24817cbaebfd
mishchuk <mishchuksg@gmail.com>
2024-11-23 21:35:35 +0300

    all done

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import re
 def clear_names(file_name: str) -> list:
     """Функция для очистки имен от лишних символов"""
     new_name_lists = list()
-    with open ('data/' + file_name, 'r') as names_file:
+    with open ('data/' + file_name, 'r', encoding="utf-8") as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
             new_name = ''
@@ -20,7 +20,7 @@ def is_cyrillic(name_item: str) -> bool:
 
     return bool(re.search('[а-яА-Я]', name_item))
 
-def russian_names_list(names_list: list) ->list:
+def filter_russian_names(names_list: list) ->list:
     """Фильтрация имен написанных на русском"""
     new_names_list = list()
     for name_item in names_list:
@@ -28,15 +28,32 @@ def russian_names_list(names_list: list) ->list:
             new_names_list.append(name_item)
     return new_names_list
 
+
+def filter_english_names(names_list: list) ->list:
+    """Фильтрация имен написанных на английском"""
+    new_names_list = list()
+    for name_item in names_list:
+        if not is_cyrillic(name_item):
+            new_names_list.append(name_item)
+    return new_names_list
+
+
 def save_to_file(file_name: str, data: str ) -> None:
     """"Сохраняет данные в файл"""
 
-    with open('data/' + file_name, 'w') as names_file:
+    with open('data/' + file_name, 'w', encoding="utf-8") as names_file:
         names_file.write(data)
 
 
 if __name__ == '__main__':
     cleared_name = clear_names('names.txt')
 
-    filtred_names = russian_names_list(cleared_name)
-    save_to_file('russian_names.txt', '\n'.join(filtred_names))
+    filtred_names = filter_russian_names(cleared_name)
+    save_to_file(
+        'russian_names.txt',
+        '\n'.join(filtred_names))
+
+    filtred_names = filter_english_names(cleared_name)
+    save_to_file(
+        'english_names.txt',
+        '\n'.join(filtred_names))
